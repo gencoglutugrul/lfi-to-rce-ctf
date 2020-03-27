b11cb1650013dd18f69e82cd058e78705ad2beb5
Tuğrul Gençoğlu <gencoglutugrul@gmail.com>
2020-03-27 22:59:07 +0300

    typo

--- src/services.php
+++ src/services.php
@@ -11,7 +11,7 @@
       <section class="py-5">
         <div class="container">
           <h1>Yeah thats right...</h1>
-          <p class="lead">Not to touch the earth not to see the son.</p>
+          <p class="lead">Not to touch the earth not to see the sun.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
         </div>
       </section>
